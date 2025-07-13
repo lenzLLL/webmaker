f67d67ce03a150f72291d74e42da3ae1ef3a5810
lenzLLL <70536398+lenzLLL@users.noreply.github.com>
2025-07-13 17:41:32 +0100

    Update route.ts 10

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -4,31 +4,27 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const body = await req.json();
   const prompt = body.prompt;
-
+const input = {
+    width: 1024,
+    height: 1024,
+    prompt
+};
   try {
-    const res: any = await replicate.run(
-      "recraft-ai/recraft-v3",
-      {
-        input: {
-           size: "1365x1024",
-          prompt,
-        },
-      }
-    );
+    const output:any = await replicate.run("black-forest-labs/flux-pro", { input });
 
-    const imageUrl = res.join("");
-   console.log(res)
+    const imageUrl = output.join("");
+   console.log(output)
     console.log(imageUrl)
     // await connectToDB();
 
     // const imageDoc = await ImageModel.create({ imageUrl, prompt });
 
-    if (!imageUrl) {
-      return NextResponse.json(
-        { error: "Failed to save image" },
-        { status: 500 }
-      );
-    }
+    // if (!imageUrl) {
+    //   return NextResponse.json(
+    //     { error: "Failed to save image" },
+    //     { status: 500 }
+    //   );
+    // }
 
     return NextResponse.json(
       { message: "Image generated successfully",url:imageUrl },
